0e3e615c6ef5d5fa6c947636b0fe5b81100bd339
Mauricio Schneider <mauricio@schneider.xyz>
2023-12-23 07:12:46 -0800

    Add bootstrap and bootstrap-icons

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-icons/font/bootstrap-icons.css'
 import './css/index.css';
 import App from './components/App';
 
