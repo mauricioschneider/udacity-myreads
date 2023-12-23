7a2a4c1bf592eacc5150c00919ba02c2de033c47
Mauricio Schneider <mauricio@schneider.xyz>
2023-12-23 12:43:35 -0800

    Add bootstrap js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.min.js'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import './css/index.css';
 import App from './components/App';
